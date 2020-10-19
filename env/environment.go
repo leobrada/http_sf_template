1638da11f7b71df0c4e580710c1f3bcdf5006632
Oleksandr Miroshkin <oleksandr.miroshkin@uni-ulm.de>
2020-10-19 22:04:14 +0200

    EDIT env. Add configuration parsing from a file

--- env/environment.go
+++ env/environment.go
@@ -1,61 +1,39 @@
 package env
 
 import (
-    "github.com/subosito/gotenv"
     "os"
-    "errors"
-    "fmt"
+    "gopkg.in/yaml.v2"
 )
 
-var (
-    // TODO: make it dynamic not hard coded
-    // SFs own certificate
-    DATA_PLANE_SF_PRIVKEY string
-    DATA_PLANE_SF_CERT string
+type Function_t struct {
+  Dns           string  `yaml:"dns"`
+  Ip            string  `yaml:"ip"`
+  Crt           string  `yaml:"crt"`
+  Key           string  `yaml:"key"`
+  Accepted    []string  `yaml:"accepted"`
+  Http_header   string  `yaml:"http_header"`
+}
 
-    // concatenated PEM file with certs accepted from SF
-    DATA_PLANE_ACCEPTED_CERTS string
+type Config_t struct {
+  Functions []Function_t
+  Listen_port  string  `yaml:"listen_port"`
+}
 
-    ROUTER_LISTEN_ADDR string
-)
+var Config Config_t
 
-func GetCertAndKeyByEnvName(key_path, cert_path string) (pkey, cert string, err error) {
-    if pkey, err = LoadEnv(key_path); err != nil {
+// Parses a configuration yaml file into the global Config variable
+func LoadConfig(configPath string) (err error) {
+    // Open config file
+    file, err := os.Open(configPath)
+    if err != nil {
         return
     }
-    if cert, err = LoadEnv(cert_path); err != nil {
-        return
-    }
+    defer file.Close()
+    
+    // Init new YAML decode
+    d := yaml.NewDecoder(file)
+
+    // Start YAML decoding from file
+    err = d.Decode(&Config)
     return
-}
-
-func GetCertByEnvName(cert_path string) (cert string, err error) {
-    if cert, err = LoadEnv(cert_path); err != nil {
-        return
-    }
-    return
-}
-
-func LoadRouterListenAddr() {
-    ROUTER_LISTEN_ADDR = LoadEnvWithDefault("ROUTER_LISTEN_ADDR", ":443")
-}
-
-func LoadEnv(key string) (string, error) {
-    value, ok := os.LookupEnv(key)
-    if !ok {
-        return "", errors.New(fmt.Sprintf("Error: Could not load %s", key))
-    }
-    return value, nil
-}
-
-func LoadEnvWithDefault(key, _default string) string {
-    if value, ok := os.LookupEnv(key); ok {
-        return value
-    }
-    return _default
-}
-
-func InitEnv() error {
-    err := gotenv.Load();
-    return err
 }
